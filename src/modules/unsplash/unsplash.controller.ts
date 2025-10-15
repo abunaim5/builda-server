700f59d459ccd3ed142d58eec09e4e95eb317568
Abu Naim <abunaim5@users.noreply.github.com>
2025-10-15 23:48:04 +0600

    added unsplash module with unsplash service and controller file to fetch images

---
+++ src/modules/unsplash/unsplash.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { getRandomImages } from './unsplash.service';
+
+export const getRandomImagesController = async (_req: Request, res: Response) => {
+    try {
+        const images = await getRandomImages();
+        res.status(200).json({ images: images });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to fetch images' });
+    };
+};
---
+++ src/modules/unsplash/unsplash.service.ts
@@ -0,0 +1,11 @@
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+    accessKey: process.env.UNSPLASH_ACCESS_KEY!,
+    fetch: fetch
+});
+
+export const getRandomImages = async () => {
+    const result = await unsplash.photos.getRandom({ count: 20 });
+    return result.response;
+};
