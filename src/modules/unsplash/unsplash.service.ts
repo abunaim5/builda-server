bd50ab62c0a69fe4590ee66972c228097916b90c
Abu Naim <abunaim5@users.noreply.github.com>
2025-10-18 01:42:33 +0600

    fix Unsplash API Error: ['OAuth error: The access token is invalid']

--- src/modules/unsplash/unsplash.service.ts
+++ src/modules/unsplash/unsplash.service.ts
@@ -1,15 +1,26 @@
 import { createApi } from "unsplash-js";
+import nodeFetch from 'node-fetch';
 
 const unsplash = createApi({
     accessKey: process.env.UNSPLASH_ACCESS_KEY!,
-    fetch: fetch
+    fetch: nodeFetch as any,
 });
 
 export const getRandomImages = async () => {
-    const result = await unsplash.photos.getRandom({
-        count: 20,
-        collectionIds: ['317099']
-    });
-    console.log(result.response)
-    return result.response;
+    try {
+        const result = await unsplash.photos.getRandom({
+            count: 20,
+            collectionIds: ['317099'],
+        });
+
+        if (result.errors) {
+            console.error('Unsplash API Error:', result.errors);
+            return null;
+        };
+
+        return result.response;
+    } catch (err) {
+        console.error('Unsplash API Error:', err);
+        return null;
+    }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
+// import 'dotenv/config'
 import app from "./app";
-import dotenv from 'dotenv';
 
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Builda server running on port ${PORT}`));
